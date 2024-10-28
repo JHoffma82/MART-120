ac895acc11f65965b35337c67b1ec6b28a7bb5e3
JHoffma82 <coupdegrass82@gmail.com>
2024-10-28 12:16:29 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,51 @@
+
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(120,45,78);
+
+    
+    fill(245,165,30);
+    circle(250,100,175);
+    
+    fill(10,124,130);
+    rect(200,187,100,150);
+
+    strokeWeight(10);
+    fill(150, 79, 80);
+    point(200,75);
+    point(285,75);
+
+    point(245,100);
+
+    ellipse(245,135,30,45);
+
+    line(145,95,175,45);
+    line(325,45,357,90);
+
+    fill(10,24,120);
+    rect(200,185,100,50);
+
+    fill(175);
+    triangle(220,320,250,220,280,320);
+
+    fill(10,25,120);
+    rect(300,195,50,10);
+
+    rect(150,195,50,10);
+
+    rect(150,195,50,10);
+
+    rect(200,335,10,50);
+
+    rect(290,335,10,50);
+
+    fill(175);
+    textSize(22);
+    text("Jared Hoffman", 270, 500);
+
+}
